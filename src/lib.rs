b7f99b878e227c5355585e84000ff137b67ef215
techyCoder81 <mtklein98@gmail.com>
2022-08-24 19:23:32 -0400

    it works!

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,13 @@
-use smashnet::*;
+#![feature(proc_macro_hygiene)]
+#![feature(allocator_api)]
+//#![feature(asm)]
+#![allow(non_camel_case_types)]
+#![allow(unused_imports)]
+#![allow(dead_code)]
+#![feature(c_variadic)]
+mod curl;
 
 #[skyline::main(name = "smashnet-nro")]
 pub fn main() {
-    smashnet::install_curl();
+    curl::install_curl();
 }
