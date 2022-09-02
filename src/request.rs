48fcd1f2bf5946832a8af72fa40270ff433bac0c
techyCoder81 <mtklein98@gmail.com>
2022-09-02 13:05:48 -0400

    Update request.rs

--- src/request.rs
+++ src/request.rs
@@ -4,10 +4,10 @@ pub extern "Rust" fn get(url: String) -> Result<String, String> {
     return match minreq::get(url.clone())
         .with_header("UserAgent", "smashnet.nro")
         .send() {
-            Ok(response) => Ok(format!("{}", match response.as_str(){
-                Ok(str) => str,
-                Err(e) => format!("Error: {:?}", e).as_str()
-            })),
+            Ok(response) => match response.as_str(){
+                Ok(str) => Ok(format!("{}", str)),
+                Err(e) => Err(format!("Error: {:?}", e))
+            },
             Err(e) => Err(format!("Error: {:?}", e))
         }
 }
